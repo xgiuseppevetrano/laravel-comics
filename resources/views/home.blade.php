8c34ff0a6427609cb71d4bb02d4d56973b6bb667
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-07 20:08:14 +0200

    add main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.base')
 
 @section('title')
-    
+    Dc Comics
 @endsection
 
 @section('page-content')
-    
+    @include('partials.main')
 @endsection
---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,18 @@
+<main>
+    <section class="jumbotron"></section>
+    <section class="comics">
+        <div class="container">
+            <button class="comics__button comics__button--position">Current Series</button>
+            <div class="comics__list">
+                @foreach ($comics as $comic)
+                    <div class="comics__card">
+                        <img class="comics__card-img" src="{{asset($comic["thumb"])}}" alt="{{$comic["series"]}}">
+                        <small class="comics__card-text">{{$comic["series"]}}</small>
+                    </div>
+                @endforeach
+            </div>
+            <button class="comics__button comics__button--big">Load more</button>
+        </div>
+    </section>
+
+</main>
