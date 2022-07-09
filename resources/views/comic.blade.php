5b6c9ab97da742ceec12910740d7083448ae63e0
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-09 12:28:44 +0200

    add comic main top

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -6,6 +6,40 @@
 
 @section('page-content')
     <main>
-        <h1>{{$comic["title"]}}</h1>
+        <section class="comic">
+            <div class="container">
+                <div class="comic-top">
+                    <div class="comic-top__dx">
+                        <h1 class="comic-top__title">{{$comic["title"]}}</h1>
+                        <div class="comic-top__store">
+                            <div class="comic-top__store-dx">
+                                <span>U.S. Price: <span class="comic-top__store-price">{{$comic["price"]}}</span></span>
+                                <span class="comic-top__store-available">Available</span>
+                            </div>
+                            <div class="comic-top__store-sx">
+                                <span>Check Availability <i class="fa-solid fa-caret-down"></i></span>
+                            </div>
+                        </div>
+                        <p class="comic-top__description">{{$comic["description"]}}</p>
+                    </div>
+                    <div class="comic-top__sx">
+                        <span class="comic-top__sx-adv">Advertisement</span>
+                        <img src="{{asset('img/adv.jpg')}}" alt="adv img">
+                    </div>
+                </div>
+            </div>
+            <div class="comic-center">
+                <div class="container">
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="comic-bottom">
+                <div class="container">
+
+                </div>
+            </div>
+        </section>
     </main>
 @endsection
